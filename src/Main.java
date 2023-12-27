55543eef8c652b9da1a23da6a57f6d94804b2358
ourgit <incandle@gmail.com>
2023-12-28 00:03:05 +0800

    add scanner and write output

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @author chenjp
@@ -19,18 +20,36 @@ public class Main {
 
             // Get the output stream to send data to the server
             OutputStream outputStream = socket.getOutputStream();
-
-            // Get the input stream to receive data from the server
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             // Send a message to the server
             String messageToSend = "Hello from the client!";
             System.out.println("Sending: " + messageToSend);
             outputStream.write(messageToSend.getBytes());
             outputStream.flush();
 
-            // Receive a response from the server
-            String receivedMessage = reader.readLine();
-            System.out.println("Received: " + receivedMessage);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            InputStream inputStream = socket.getInputStream();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            while (true) {
+                try {
+                    String inputString = scanner.nextLine();
+                    System.out.println("You entered: " + inputString);
+                    outputStream.write(inputString.getBytes());
+                    outputStream.flush();
+
+                    // Get the input stream to receive data from the server
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    bytesRead = bufferedInputStream.read(buffer);
+                    if (bytesRead != -1) {
+                        System.out.println("Received " + bytesRead + " bytes: " + new String(buffer, 0, bytesRead));
+                    }
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
